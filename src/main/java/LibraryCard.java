2b8a1cc1fae9febf52849e8b1a80b5c5ccdb461e
Cameron Lindsay <s3952879@student.rmit.edu.au>
2023-10-10 15:09:02 +1100

    1st round of adding

--- src/main/java/LibraryCard.java
+++ src/main/java/LibraryCard.java
@@ -43,8 +43,8 @@ public class LibraryCard {
     public LibraryCard(Student student, Date IssueDate, Date ExpiryDate, int ID) {
         this.student = student;
         this.IssueDate = IssueDate;
-	   this.ExpiryDate = ExpiryDate;
-	   this.ID = ID;
+	    this.ExpiryDate = ExpiryDate;
+	    this.ID = ID;
     }
 
 
@@ -92,11 +92,32 @@ public class LibraryCard {
      * Issue a new book
      * @param Book: book to borrow 
      * @return true if the book is successfully borrowed, false otherwise
+     * @throws IllegalBookIssueException
      */
 
-    public boolean issueBook(Book book){
+    public boolean issueBook(Book book) throws IllegalBookIssueException{
+    
+        // Get the number of books borrowed by the student on the library card
+        int numBorrowed = borrowed.size();
+        // The number of books borrowed should not be greater than 4
+        if (numBorrowed > 4) {
+            return false;
+        }
+        // Throws an exception if the same book is already issues on the library card
+        if (borrowed.contains(book)) {
+            throw new IllegalBookIssueException("this card has already borrowed this book");
+        }
+        // Check that the library card is still valid
+        // Check that the book is available for borrowing
+        // The book should not be issued if there is pending fine associated with the library card
+        // If the above constraints are met then issue the book to the student (the method should return true) and make 
+        //    necessary update to relevant variables to reflect this change.
+        // If the above constraints are not met, then the book is not issued and the method should return false.
+        // If book to be borrowed is a high demand book then issue the book for 3 days. For a low demand book, 
+        //    it can be issued for 15 days. 
+        
     	return false;
-   
+        
     }
 
 
